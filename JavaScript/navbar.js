08c3f15908909e265ae1d4aecb5ef699aea2f420
Kady <ku.ayush@yahoo.com>
2021-12-25 14:12:03 +0530

    Used BEM convention

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -3,7 +3,7 @@ function navBar() {
     const blurTarget = document.querySelector(".container-body");
     const duplicate = blurTarget.cloneNode(true);
     const targetBlurred = document.createElement("div");
-    targetBlurred.className = "blurred-content";
+    targetBlurred.className = "navbar__blurcontent";
     targetBlurred.appendChild(duplicate);
     const navContainer = document.querySelector(".navbar");
     navContainer.appendChild(targetBlurred);
@@ -11,12 +11,12 @@ function navBar() {
     const blurTargettwo = document.querySelector(".container-body");
     const duplicatetwo = blurTarget.cloneNode(true);
     const targetBlurredtwo = document.createElement("div");
-    targetBlurredtwo.className = "blurred-content";
+    targetBlurredtwo.className = "navbar__blurcontent";
     targetBlurredtwo.appendChild(duplicatetwo);
-    const ulContainer = document.querySelector(".nav-unlist");
+    const ulContainer = document.querySelector(".navbar__btn");
     ulContainer.appendChild(targetBlurredtwo);
 
-    const contentWrapper = document.querySelector(".blurred-content");
+    const contentWrapper = document.querySelector(".navbar__blurcontent");
     window.addEventListener('scroll', function () {
         duplicate.style.marginTop = -this.scrollY + 'px';
         duplicatetwo.style.marginTop = -this.scrollY + 'px';
